b871401e904fe0fc29b89e7f62ea3f09e3a9e006
seji <atienza.seji@gmail.com>
2022-09-24 22:51:43 +0800

    fix: Footer Nav not displaying public links

--- Client/src/components/layouts/Footer.jsx
+++ Client/src/components/layouts/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Nav from '@/components/Nav';
+import { NAV_TYPES as types } from '@/constants/getTypes';
 
 export default function Footer() {
   return (
     <footer className='container'>
-      <Nav />
+      <Nav type={types.PUBLIC}/>
     </footer>
   );
 }
