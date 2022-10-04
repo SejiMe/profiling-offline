2a71c9ac14c7ab0e7fb9e55f2dbd34cc67d19dff
seji <atienza.seji@gmail.com>
2022-10-04 10:20:24 +0800

    build: checkbox functionalities

--- Client/src/components/Checkbox.jsx
+++ Client/src/components/Checkbox.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from 'react';
 import clsx from 'clsx';
 //TODO Input Handler for Changes
-export default function Checkbox({children, val='', name='', iClassName = '',}) {
+export default function Checkbox({
+  children,
+  val = '',
+  checkVal = false,
+  getValue,
+  name = '',
+  iClassName = '',
+}) {
+  const handleCheckbox = (e) => {
+    const { name, value, checked } = e.target;
+    getValue({
+      name,
+      value,
+      checked,
+    });
+  };
+
   return (
     <div className='flex flex-row items-center'>
-        <input type="checkbox" value={val} name={name} className={clsx('rounded-3xl hover:cursor-pointer mr-1', iClassName)} />
-        <label htmlFor={name}>{children}</label>
+      <input
+        type='checkbox'
+        value={val}
+        name={name}
+        checked={checkVal}
+        onChange={handleCheckbox}
+        className={clsx('rounded-3xl hover:cursor-pointer mr-1', iClassName)}
+      />
+      <label htmlFor={name}>{children}</label>
     </div>
-  )
+  );
 }
