9fd01754bcab85774809bf7c7c47694021ac133f
seji <atienza.seji@gmail.com>
2022-10-05 09:02:12 +0800

    build: add config svgr/weback

--- Client/next.config.js
+++ Client/next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  rules: [
+    {
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    },
+  ],
   eslint: {
     dirs: ['src'],
   },
-  
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
