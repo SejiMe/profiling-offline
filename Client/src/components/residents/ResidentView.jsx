026e177dff092374ea0c6204b3265358015ca83d
seji <atienza.seji@gmail.com>
2022-10-06 21:54:49 +0800

    build: Initial Read Data & Add Query Client

--- Client/src/components/residents/ResidentView.jsx
+++ Client/src/components/residents/ResidentView.jsx
@@ -1,17 +1,44 @@
 import React from 'react';
 import ResidentItem from './ResidentItem';
+import { useQuery } from 'react-query';
+import { db } from '@/config/firebaseConfig';
+import { collection, getDocs } from 'firebase/firestore';
 
 const ResidentView = ({ label = 'Feature' }) => {
+  const { isLoading, data } = useQuery('residents', async () => {
+    const colRef = collection(db, 'residents');
+    const snapshots = await getDocs(colRef);
+    console.log(snapshots);
+    const documents = snapshots.docs.map((doc) => {
+      const data = doc.data();
+      data.id = doc.id;
+      return data;
+    });
+    console.log(documents);
+    return documents;
+  });
+
+  if (isLoading) {
+    return (
+      <>
+        <h2>Loading...</h2>
+      </>
+    );
+  }
+
   return (
     <div className='h-full w-full'>
       <input type='text' placeholder='Search Resident Name' />
       <h2>{label}</h2>
       <div className=''>
         <ul className='flex flex-col gap-1'>
-          <ResidentItem />
-          <ResidentItem />
-          <ResidentItem />
-          <ResidentItem />
+          {data?.map((document) => (
+            <ResidentItem
+              key={document.id}
+              id={document.id}
+              name={document.firstName}
+            />
+          ))}
         </ul>
       </div>
     </div>
--- Client/src/pages/_app.js
+++ Client/src/pages/_app.js
@@ -2,24 +2,27 @@ import { ProtectedRoutes } from '@/components/ProtectedRoutes';
 import { AuthContextProvider } from '@/contexts/AuthContext';
 import '@/styles/globals.css';
 import { useRouter } from 'next/router';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const noAuthRequired = ['/', '/login', '/contact', '/services'];
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-
+  const queryClient = new QueryClient();
   return (
     /* Checking if the pathname is in the noAuthRequired array. If it is, it renders the component. If
     it is not, it renders the ProtectedRoutes component. */
-    <AuthContextProvider>
-      {noAuthRequired.includes(router.pathname) ? (
-        <Component {...pageProps} />
-      ) : (
-        <ProtectedRoutes>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        {noAuthRequired.includes(router.pathname) ? (
           <Component {...pageProps} />
-        </ProtectedRoutes>
-      )}
-    </AuthContextProvider>
+        ) : (
+          <ProtectedRoutes>
+            <Component {...pageProps} />
+          </ProtectedRoutes>
+        )}
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
 
