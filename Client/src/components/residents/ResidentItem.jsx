87252e5756b62b58281e24f7fd8df5dc2f74366f
seji <atienza.seji@gmail.com>
2022-10-12 19:35:18 +0800

    build: new feat for viewing information in admin page

--- Client/src/components/residents/ResidentItem.jsx
+++ Client/src/components/residents/ResidentItem.jsx
@@ -1,10 +1,21 @@
+import Link from 'next/link';
 import React from 'react';
+import Info from './../svg/icons8-info-26.svg';
 
-const ResidentItem = ({ name, key }) => {
+const ResidentItem = (props) => {
   return (
     <>
-      <li className='w-full h-10 px-2 py-1 bg-slate-300 hover:border-l-8 hover:border-green-700 '>
-        <h4>Resident Name</h4>
+      <li className='flex justify-between w-[80%] gap-3 pr-10 h-10 px-2 py-1 bg-slate-300 hover:border-l-8 hover:border-green-700 '>
+        <h4>{props.id}</h4>
+        <h4>{props.fname}</h4>
+        <h4>{props.mName}</h4>
+        <h4>{props.lname}</h4>
+        {/* ICON Should be here */}
+        <Link href={'/admin/residents/' + props.id} className=''>
+          <a>
+            <Info />
+          </a>
+        </Link>
       </li>
     </>
   );
