e230655b890f8818f4274071e53bd8481031ae7f
seji <atienza.seji@gmail.com>
2022-10-17 20:16:52 +0800

    refactor: change component name login -> Login

--- Client/src/pages/login.jsx
+++ Client/src/pages/login.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { useAuth } from '@/contexts/AuthContext';
-export default function login() {
+export default function Login() {
   const [data, setData] = useState({
     email: '',
     password: '',
