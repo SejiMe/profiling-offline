45e30504796338b84ec1f3c76ac0ed3cefb8aa48
seji <atienza.seji@gmail.com>
2022-10-31 14:56:13 +0800

    chore: added className on parameters for tailwind

--- Client/src/components/layouts/Container.jsx
+++ Client/src/components/layouts/Container.jsx
@@ -1,33 +1,41 @@
 import React from 'react';
 import { VIEW_TYPES as types } from '@/constants/getTypes';
+import clsx from 'clsx';
 
-const Container = ({ type = types.DEFAULT, children }) => {
+const Container = ({ type = types.DEFAULT, children, className }) => {
   return (
     <>
-      {type === types.DEFAULT && <DefaultView>{children}</DefaultView>}
-      {type === types.RESIDENT && <ResidentView>{children}</ResidentView>}
+      {type === types.DEFAULT && (
+        <DefaultView className={className}>{children}</DefaultView>
+      )}
+      {type === types.RESIDENT && (
+        <ResidentView className={className}>{children}</ResidentView>
+      )}
     </>
   );
 };
 
 export default Container;
 
-function DefaultView({ children }) {
+function DefaultView({ children, className }) {
   return (
-    <main className='bg-slate-100 px-2 py-1 h-screen w-min-full'>
-      <div className=' bg-slate-200 flex flex-col items-center justify-center  p-4'>
-        {children}
-      </div>
+    <main
+      className={clsx(
+        'px-2 py-1 w-full h-full md:flex md:flex-row rounded-sm',
+        className
+      )}
+    >
+      {children}
     </main>
   );
 }
 
-function ResidentView({ children }) {
+function ResidentView({ children, className }) {
   return (
-    <main className='bg-green-100 px-8 py-8 h-screen w-min-full'>
-      <div className=' bg-slate-100 flex flex-col min-w-fill min-h-full rounded-lg  p-4'>
-        {children}
-      </div>
+    <main
+      className={clsx('bg-green-100 px-8 py-8 h-screen w-min-full', className)}
+    >
+      {children}
     </main>
   );
 }
