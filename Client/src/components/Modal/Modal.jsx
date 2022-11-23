2231906c8cb7c258f87d2fe90bb93b7a07c1a1eb
seji <atienza.seji@gmail.com>
2022-11-23 21:31:36 +0800

    build: classnames for modal

--- Client/src/components/Modal/Modal.jsx
+++ Client/src/components/Modal/Modal.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import styles from '@/styles/Modal.module.css';
 import clsx from 'clsx';
 
-export default function Modal({ show, onClose, children }) {
+export default function Modal({ show, onClose, children, stylesBody = '' }) {
   const [isBrowser, setIsBrowser] = useState(false);
   useEffect(() => {
     setIsBrowser(true);
@@ -16,13 +16,13 @@ export default function Modal({ show, onClose, children }) {
 
   const modalContent = show ? (
     <div className={styles.overlay}>
-      <div className={clsx(styles.modal, 'flex justify-center content-center')}>
+      <div className={clsx(styles.modal, '')}>
         <div className={clsx(styles.header, '')}>
           <a href='#' onClick={handleClose}>
             <button>Close</button>
           </a>
         </div>
-        <div className={clsx(styles.body, '')}>{children}</div>
+        <div className={clsx(styles.body, stylesBody)}>{children}</div>
       </div>
     </div>
   ) : null;
