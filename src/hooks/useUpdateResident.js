fb914205ba0029e2c1153c7d0e0fd4a2e760b0bb
seji <atienza.seji@gmail.com>
2022-11-27 17:27:29 +0800

    chore: remove console logs

--- src/hooks/useUpdateResident.js
+++ src/hooks/useUpdateResident.js
@@ -3,11 +3,7 @@ import { db } from '@/config/firebaseConfig';
 import { useMutation } from 'react-query';
 
 const updateResident = async ({ residentDoc, residentID }) => {
-  console.log(residentDoc);
-  console.log(residentID);
-
   const docReference = doc(db, 'residents', residentID);
-
   return await setDoc(docReference, residentDoc);
 };
 
