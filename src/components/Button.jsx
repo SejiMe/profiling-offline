2a481b144b9f653b22c375b6c175559e00d5f1d7
seji <atienza.seji@gmail.com>
2022-12-05 08:51:09 +0800

    chore: fixed classNames

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import clsx from 'clsx';
 
-export default function Button({ children, className = '', ...rest }) {
+export default function Button({ children, className, ...rest }) {
   return (
     <button
       {...rest}
       className={clsx(
-        'rounded p-2 text-center font-bold hover:text-primary-500 bg-green-400 animated-underline',
+        'rounded p-2 text-black text-center font-bold hover:text-primary-500 bg-green-400 animated-underline',
         className
       )}
     >
