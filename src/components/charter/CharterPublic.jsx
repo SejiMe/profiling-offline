d2d4cbc6d5f7f82cb5574595c2a1f8f67208ca47
seji <atienza.seji@gmail.com>
2022-12-13 21:55:54 +0800

    chore: improve ui charter

--- src/components/charter/CharterPublic.jsx
+++ src/components/charter/CharterPublic.jsx
@@ -24,63 +24,71 @@ const CharterPublic = () => {
       <Element name='about'>
         <h2 className='text-center mt-2'>Barangay Officials</h2>
       </Element>
-      <div className='p-10 flex flex-col '>
+      <div className='p-10 flex flex-col justify-center items-center md:grid md:grid-cols-6 gap-3 align-middle'>
         <OfficiateCard
           className={
-            'col-start-1 col-end-3 bg-slate-50 p-5 border-2 border-black'
+            'col-start-2 col-end-6 bg-slate-50 p-5 border-2 border-black'
           }
           name={data.Captain}
           position={'Barangay Captain'}
         />
         <OfficiateCard
           className={
-            'col-start-1 col-span-1 bg-slate-50 p-5 border-2 border-black'
+            'col-start-1 col-span-3 bg-slate-50 p-5 border-2 border-black'
           }
           name={data.Secretary}
           position={'Barangay Secretary'}
         />
         <OfficiateCard
-          className={'col-span-1 p-5 bg-slate-50 border-2 border-black'}
+          className={
+            'col-start-4 col-span-3 p-5 bg-slate-50 border-2 border-black'
+          }
           name={data.Treasurer}
           position={'Barangay Treasurer'}
         />
         <OfficiateCard
-          className={'col-start-1 p-5 bg-slate-50 border-2 border-black'}
+          className={
+            'col-start-1 col-span-2 p-5 bg-slate-50 border-2 border-black'
+          }
           name={data.Kagawad1}
           position={'Barangay Kagawad'}
         />
         <OfficiateCard
-          className={'p-5 bg-slate-50 border-2 border-black'}
+          className={'col-span-2 p-5 bg-slate-50 border-2 border-black'}
           name={data.Kagawad2}
           position={'Barangay Kagawad'}
         />
         <OfficiateCard
-          className={'p-5 bg-slate-50 border-2 border-black'}
+          className={'col-span-2 p-5 bg-slate-50 border-2 border-black'}
           name={data.Kagawad3}
           position={'Barangay Kagawad'}
         />
         <OfficiateCard
-          className={'col-start-1 p-5 bg-slate-50 border-2 border-black'}
+          className={'col-span-2 p-5 bg-slate-50 border-2 border-black'}
           name={data.Kagawad4}
           position={'Barangay Kagawad'}
         />
         <OfficiateCard
-          className={'p-5 bg-slate-50 border-2 border-black'}
+          className={'col-span-2 p-5 bg-slate-50 border-2 border-black'}
           name={data.Kagawad5}
           position={'Barangay Kagawad'}
         />
         <OfficiateCard
-          className={'p-5 bg-slate-50 border-2 border-black'}
+          className={'col-span-2 p-5 bg-slate-50 border-2 border-black'}
           name={data.Kagawad6}
           position={'Barangay Kagawad'}
         />
         <OfficiateCard
-          className={'col-start-2 p-5 bg-slate-50 border-2 border-black'}
+          className={
+            'col-start-3 col-span-2 p-5 bg-slate-50 border-2 border-black'
+          }
           name={data.Kagawad7}
           position={'Barangay Kagawad'}
         />
         <OfficiateCard
-          className={'col-start-2 p-5 bg-slate-50 border-2 border-black'}
+          className={
+            'col-start-3 col-span-2 p-5 bg-slate-50 border-2 border-black'
+          }
           name={data.SK}
           position={'SK Chairman'}
         />
